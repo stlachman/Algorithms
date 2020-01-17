e7600ad0f045e81d94cae57c8edd0c292d900189
Seth Lachman <lachmanseth17@gmail.com>
2020-01-17 18:30:54 -0500

    Added rock paper scissors solution and recipe batches solutions.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,29 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+
+
+def eating_cookies(amount, cache=None):
+    if amount == 0:
+        return 1
+    print(amount)
+    total = 0
+    if amount - 3 >= 0:
+        total += eating_cookies(amount-3)
+    if amount - 2 >= 0:
+        total += eating_cookies(amount-2)
+    if amount - 1 >= 0:
+        total += eating_cookies(amount-1)
+
+    return total
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,23 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    current_min = float("inf")
+
+    if len(ingredients.keys()) != len(recipe.keys()):
+        return 0
+
+    for ing in ingredients.keys():
+        if ingredients[ing] // recipe[ing] < current_min:
+            current_min = ingredients[ing] // recipe[ing]
+    return current_min
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
